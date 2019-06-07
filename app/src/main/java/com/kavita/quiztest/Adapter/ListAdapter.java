65f24a33bc798610000944c7076dcfd3d2543120
Ranger12021999 <amansharma123a@gmail.com>
2019-06-07 13:40:35 +0530

    SplashScreen Completed 4

--- app/src/main/java/com/kavita/quiztest/Adapter/ListAdapter.java
+++ app/src/main/java/com/kavita/quiztest/Adapter/ListAdapter.java
@@ -1,18 +1,24 @@
 package com.kavita.quiztest.Adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.load.engine.Resource;
 import com.kavita.quiztest.Models.ListItem;
 import com.kavita.quiztest.R;
 import com.kavita.quiztest.UI.Fragments.DashboardFragment;
@@ -41,6 +47,14 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ListAdapter.ViewHolder holder, int position) {
         ListItem listItem = listItems.get(position);
         holder.tv.setText(listItem.getTest_name());
+        if(position%2==0)
+        {
+            holder.linearLayout.setBackgroundResource(R.drawable.text_gradient);
+        }
+        else
+        {
+            holder.linearLayout.setBackgroundResource(R.drawable.text_gradient1);
+        }
     }
 
     @Override
@@ -50,11 +64,12 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView tv;
-
+        private LinearLayout linearLayout;
 
         private ViewHolder(@NonNull final View view) {
             super(view);
             tv = view.findViewById(R.id.text_list);
+            linearLayout=view.findViewById(R.id.Linear);
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
